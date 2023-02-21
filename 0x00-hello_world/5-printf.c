b5db0d298fdf2315a7c6e7d7ca4b1013ede74b50
Edith <edithogada.eo@gmail.com>
2023-02-21 06:38:04 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 /**
- * main ()
- * {
- * printF("with proper grammar, but the outcome is a piece of art, /n");
- * return 0;
- * }
+* main - print a C program using printF
+*
+* Return (0)
+*/
+int main(void)
+{
+	printF("with proper grammar, but the outcome is a piece of art,/n");
+	return 0;
+}
