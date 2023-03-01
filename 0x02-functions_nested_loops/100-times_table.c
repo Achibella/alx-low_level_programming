e0561e44cb7bb0ac18b408a83ca44388d1f4c4a0
Edith <edithogada.eo@gmail.com>
2023-02-28 21:04:57 -0800

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,46 +2,46 @@
 
 /**
  * print_times_table - prints the n times table, starting with 0
- * @n: the number to print the times table of
+ * @n: number of the times table
  */
 void print_times_table(int n)
 {
-	if (n > 15 || n < 0)
-		return;
+	int i, j, k;
 
-	int i, j, product;
-
-	for (i = 0; i <= n; i++)
+	if (n >= 0 && n <= 15)
 	{
-		for (j = 0; j <= n; j++)
+		for (i = 0; i <= n; i++)
 		{
-			product = i * j;
-			if (j == 0)
-				_putchar('0' + product);
-			else
+			for (j = 0; j <= n; j++)
 			{
-				_putchar(',');
-				_putchar(' ');
-				if (product < 10)
+				k = j * i;
+				if (j == 0)
 				{
+					_putchar(k + '0');
+				} else if (k < 10 && j != 0)
+				{
+					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
-					_putchar('0' + product);
-				}
-				else if (product < 100)
-				{
 					_putchar(' ');
-					_putchar('0' + (product / 10));
-					_putchar('0' + (product % 10));
-				}
-				else
+					_putchar(k + '0');
+				} else if (k >= 10 && k < 100)
 				{
-					_putchar('0' + (product / 100));
-					_putchar('0' + ((product / 10) % 10));
-					_putchar('0' + (product % 10));
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
+				} else if (k >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((k / 100) + '0');
+					_putchar(((k / 10) % 10) + '0');
+					_putchar((k % 10) + '0');
 				}
 			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
